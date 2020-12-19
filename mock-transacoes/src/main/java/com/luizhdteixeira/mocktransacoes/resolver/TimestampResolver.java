f83c33dc960151ddfc52775d32804f61a6b603e2
luizhdteixeira <luiz.teixeira5@fatec.sp.gov.br>
2020-12-18 23:09:02 -0300

    code

--- mock-transacoes/src/main/java/com/luizhdteixeira/mocktransacoes/resolver/TimestampResolver.java
+++ mock-transacoes/src/main/java/com/luizhdteixeira/mocktransacoes/resolver/TimestampResolver.java
@@ -5,8 +5,8 @@ import java.sql.Timestamp;
 public class TimestampResolver {
 
     public long randomTimestamp(int month, int year) {
-        long offset = Timestamp.valueOf(year + "-" + month + "-30 00:00:00").getTime();
-        long end = Timestamp.valueOf(year + "-" + month + "-01 00:00:00").getTime();
+        long offset = Timestamp.valueOf(year + "-" + month + "-01 00:00:00").getTime();
+        long end = Timestamp.valueOf(year + "-" + month + "-30 00:00:00").getTime();
         long diff = end - offset + 1;
         return new Timestamp(offset + (long) (Math.random() * diff)).getTime();
     }
---
+++ mock-transacoes/src/test/java/com/luizhdteixeira/mocktransacoes/MockTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizhdteixeira.mocktransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
