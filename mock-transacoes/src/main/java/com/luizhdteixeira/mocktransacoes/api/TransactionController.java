8782bdfa2f53e81911b7eedb97755cc827540497
luizhdteixeira <luiz.teixeira5@fatec.sp.gov.br>
2020-12-18 21:07:46 -0300

    Ajustes de validação

--- mock-transacoes/src/main/java/com/luizhdteixeira/mocktransacoes/api/TransactionController.java
+++ mock-transacoes/src/main/java/com/luizhdteixeira/mocktransacoes/api/TransactionController.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 public interface TransactionController {
 
-    @GetMapping(path = "/{id}/transacoes/{mes}/{ano}", consumes = "application/json", produces = "application/json")
+    @GetMapping(path = "/{id}/transacoes/{ano}/{mes}", consumes = "application/json", produces = "application/json")
     ResponseEntity<List<TransactionDTO>> findByParamsTransactions(
             @PathVariable(value = "id") @Min(value = 4, message = "{id.min}") @Max(value = 6, message = "{id.max}") @NotBlank(message = "{id.not.blank}") Integer id,
-            @PathVariable(value = "mes") @Min(value = 4, message = "{month.min}") @Max(value = 6, message = "{month.max}") @NotBlank(message = "{month.not.blank}") Integer month,
-            @PathVariable(value = "ano") @Min(value = 4, message = "{year.min}") @Max(value = 6, message = "{year.max}") @NotBlank(message = "{year.not.blank}") Integer year
+            @PathVariable(value = "mes") @Min(value = 1, message = "{month.min}") @Max(value = 2, message = "{month.max}") @NotBlank(message = "{month.not.blank}") Integer month,
+            @PathVariable(value = "ano") @Min(value = 2, message = "{year.min}") @Max(value = 4, message = "{year.max}") @NotBlank(message = "{year.not.blank}") Integer year
     );
 }
